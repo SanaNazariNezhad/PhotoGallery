6d97d6c77c2ee8dc446af1fcbe9da248da2305b0
SanaNazariNezhad <sana.nazari1995@gmail.com>
2020-11-05 11:43:14 +0330

    Using picasso instead of my custom message loop. minor refactor.

--- app/src/main/java/org/maktab/photogallery/controller/fragment/PhotoGalleryFragment.java
+++ app/src/main/java/org/maktab/photogallery/controller/fragment/PhotoGalleryFragment.java
@@ -1,8 +1,5 @@
 package org.maktab.photogallery.controller.fragment;
 
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
@@ -11,35 +8,29 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Handler;
-import android.util.LruCache;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import org.maktab.photogallery.R;
 import org.maktab.photogallery.controller.EndlessRecyclerViewScrollListener;
 import org.maktab.photogallery.model.GalleryItem;
 import org.maktab.photogallery.repository.PhotoRepository;
-import org.maktab.photogallery.service.ThumbnailDownloader;
 
-import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PhotoGalleryFragment extends Fragment {
 
     private static final int SPAN_COUNT = 3;
-    private static final String TAG = "PGF";
     private RecyclerView mRecyclerView;
     private PhotoRepository mRepository;
     private int mCount;
     private ProgressBar mProgressBar;
-    private LruCache<String, Bitmap> memoryCache;
-    private ThumbnailDownloader<PhotoHolder> mThumbnailDownloader;
     List<GalleryItem> mItems;
     private int mCurrentItem;
 
@@ -60,89 +51,11 @@ public class PhotoGalleryFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
-        final int cacheSize = maxMemory / 8;
         mItems = new ArrayList<>();
         mCurrentItem = 0;
         mRepository = new PhotoRepository();
         mCount = 1;
-
-        setupThumbnailDownloader();
-
-        memoryCache = new LruCache<String, Bitmap>(cacheSize) {
-            @Override
-            protected int sizeOf(String key, Bitmap bitmap) {
-                // The cache size will be measured in kilobytes rather than
-                // number of items.
-                return bitmap.getByteCount() / 1024;
-            }
-        };
-
-        /*FlickrTask flickrTask = new FlickrTask();
-        flickrTask.execute();*/
-
-        /*Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                FlickrFetcher flickrFetcher = new FlickrFetcher();
-                try {
-                    String response = flickrFetcher.getUrlString("https://www.digikala.com/");
-                    Log.d(TAG, response);
-
-                    getActivity().runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            mTextView.setText(response);
-                        }
-                    });
-                } catch (IOException e) {
-                    Log.e(TAG, e.getMessage(), e);
-                }
-            }
-        });
-        thread.start();*/
     }
-
-    private void setupThumbnailDownloader() {
-        Handler uiHandler = new Handler();
-
-        mThumbnailDownloader = new ThumbnailDownloader(uiHandler);
-        mThumbnailDownloader.start();
-        mThumbnailDownloader.getLooper();
-        mThumbnailDownloader.setListener(
-                new ThumbnailDownloader.ThumbnailDownloaderListener<PhotoHolder>() {
-                    @Override
-                    public void onThumbnailDownloaded(PhotoHolder target, Bitmap bitmap) {
-                        target.bindBitmap(bitmap);
-                    }
-                });
-    }
-
-    /*public void addBitmapToMemoryCache(String key, Bitmap bitmap) {
-        if (getBitmapFromMemCache(key) == null) {
-            memoryCache.put(key, bitmap);
-        }
-    }
-
-    public Bitmap getBitmapFromMemCache(String key) {
-        return memoryCache.get(key);
-    }*/
-
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-
-        mThumbnailDownloader.quit();
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-
-        mThumbnailDownloader.clearQueue();
-    }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -197,7 +110,7 @@ public class PhotoGalleryFragment extends Fragment {
     private class PhotoHolder extends RecyclerView.ViewHolder {
 
         private ImageView mImageViewItem;
-        private GalleryItem mItem;
+
 
         public PhotoHolder(@NonNull View itemView) {
             super(itemView);
@@ -206,35 +119,12 @@ public class PhotoGalleryFragment extends Fragment {
         }
 
         public void bindGalleryItem(GalleryItem item) {
-            mItem = item;
-            mImageViewItem.setImageDrawable(
-                    getResources().getDrawable(R.mipmap.ic_placeholder));
 
-            //queue the message for download
-            mThumbnailDownloader.queueThumbnail(this, item.getUrl());
+            Picasso.get()
+                    .load(item.getUrl())
+                    .placeholder(R.mipmap.ic_android_placeholder)
+                    .into(mImageViewItem);
         }
-
-        public void bindBitmap(Bitmap bitmap) {
-//            loadBitmap(bitmap.getGenerationId(),mImageViewItem);
-            Bitmap mBitmap = MyCache.getInstance().retrieveBitmapFromCache(ThumbnailDownloader.BITMAP_KEY);
-            if (mBitmap != null)
-                mImageViewItem.setImageBitmap(mBitmap);
-            else
-                mImageViewItem.setImageBitmap(bitmap);
-        }
-
-       /* public void loadBitmap(int resId, ImageView imageView) {
-            final String imageKey = String.valueOf(resId);
-
-            final Bitmap bitmap = getBitmapFromMemCache(imageKey);
-            if (bitmap != null) {
-                imageView.setImageBitmap(bitmap);
-            } else {
-                imageView.setImageResource(R.mipmap.ic_android_placeholder);
-                BitmapWorkerTask task = new BitmapWorkerTask(imageView);
-                task.execute(resId);
-            }
-        }*/
     }
 
     private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
@@ -299,76 +189,4 @@ public class PhotoGalleryFragment extends Fragment {
         }
 
     }
-
-   /* class BitmapWorkerTask extends AsyncTask<Integer, Void, Bitmap> {
-
-//        private final WeakReference<ImageView> imageViewReference;
-        private ImageView mImageView;
-        private int data = 0;
-        public BitmapWorkerTask(ImageView imageView) {
-            // Use a WeakReference to ensure the ImageView can be garbage collected
-//            imageView = new WeakReference<ImageView>(imageView);
-            mImageView = imageView;
-        }
-
-        // Decode image in background.
-
-        @Override
-        protected Bitmap doInBackground(Integer... params) {
-            final Bitmap bitmap = decodeSampledBitmapFromResource(
-                    getActivity().getResources(), params[0], 100, 100);
-            addBitmapToMemoryCache(String.valueOf(params[0]), bitmap);
-            return bitmap;
-        }
-        // Once complete, see if ImageView is still around and set bitmap.
-
-        @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            if (mImageView != null && bitmap != null) {
-                final ImageView imageView = mImageView;
-                if (imageView != null) {
-                    imageView.setImageBitmap(bitmap);
-                }
-            }
-        }
-    }
-
-    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
-                                                         int reqWidth, int reqHeight) {
-
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeResource(res, resId, options);
-
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeResource(res, resId, options);
-    }
-
-    public static int calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
-        if (height > reqHeight || width > reqWidth) {
-
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
-
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) >= reqHeight
-                    && (halfWidth / inSampleSize) >= reqWidth) {
-                inSampleSize *= 2;
-            }
-        }
-
-        return inSampleSize;
-    }*/
 }
